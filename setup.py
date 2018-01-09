bf806b8bcdd15beb18b1cdc5bb73695e87044602
Robert Forkel <xrotwang@googlemail.com>
2018-01-09 09:00:26 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup
 setup(
     name="pytest-clld",
     packages = ['pytest_clld'],
+    install_requires=['pytest>=3.1'],
     entry_points = {
         'pytest11': [
             'clld = pytest_clld.plugin',
