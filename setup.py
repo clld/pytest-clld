4b632f75236b1df4d1adcec15c0db705d9a17193
Robert Forkel <xrotwang@googlemail.com>
2024-10-23 11:26:57 +0200

    bumped version number

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup
 
 setup(
     name='pytest-clld',
-    version='1.2.0',
+    version='1.3.0',
     packages=['pytest_clld'],
     install_requires=[
-        'pytest>=3.6', 
+        'pytest>=3.9', 
         'clld',
         'sqlalchemy>=1.0',
         'WebTest>=1.3.1',
